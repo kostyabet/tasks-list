c42a84431284db10ebe34f846cb2ba05dd43231a
Kostya <betenya.kostya@mail.ru>
2024-08-02 23:52:42 +0300

    try to use backend with frontend

--- frontend/tasks_list/src/App.js
+++ frontend/tasks_list/src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
+import {useState} from "react";
+import {getAllTasks} from "./services/tasks";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <p>{getAllTasks()}</p>
     </div>
   );
 }
---
+++ frontend/tasks_list/src/models/task.tsx
@@ -0,0 +1,6 @@
+﻿interface Task
+{
+    id: string;
+    title: string;
+    description: string;
+}
