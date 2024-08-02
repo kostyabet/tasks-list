b56e44c5b191324bdbb6cb5fde5845bd1a8970a5
Kostya <betenya.kostya@mail.ru>
2024-08-02 19:38:46 +0300

    fix controller 404 error
    - add app.UseRouting() and app.MapControllers()

--- TasksList/TasksList.API/Program.cs
+++ TasksList/TasksList.API/Program.cs
@@ -7,8 +7,6 @@ using TasksList.DataAccess.Repositories;
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -23,23 +21,15 @@ builder.Services.AddScoped<ITasksRepository, TasksRepository>();
 
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
+app.UseRouting();
+app.MapControllers();
+
 app.UseHttpsRedirection();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
-
 app.Run();
-
-record WeatherForecast(DateOnly Date, int TemperatureC, string? Summary)
-{
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-}
