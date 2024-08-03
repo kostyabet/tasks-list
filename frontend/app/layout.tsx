0d2943ad13507c5a7e42d99bda2e52b27b66398e
Kostya <betenya.kostya@mail.ru>
2024-08-03 21:57:40 +0300

    footer create

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,7 +1,8 @@
 import "./globals.css";
-import {Layout, Menu} from "antd";
-import {Content, Header} from "antd/es/layout/layout";
+import {Anchor, Breadcrumb, Layout, Menu} from "antd";
+import {Content, Header, Footer} from "antd/es/layout/layout";
 import Link from "next/link";
+import Home from "@/app/page";
 
 const items = [
   {key: "home", label: <Link href={"/"}>Home</Link>},
@@ -26,6 +27,9 @@ export default function RootLayout({
             />
           </Header>
           <Content style={{padding: "0 48px"}}>{children}</Content>
+          <Footer style={{ textAlign: 'center', width: "auto", flex: 1, height: "100vh", backgroundColor: "rgb(0, 21, 41)", marginTop: "25px", color: "grey"}}>
+            <p>Tasks List ©2024 Created by Kostya Betenya</p>
+          </Footer>
         </Layout>
       </body>
     </html>
